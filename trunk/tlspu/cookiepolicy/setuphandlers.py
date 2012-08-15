df6a7c0dc3b536608f9b20f6bcb33736643191ea
maurits.vanrees <maurits.vanrees@b747bfa3-e8a4-06ba-1758-46ea45de51e8>
2012-08-15 17:26:19 +0000

    Replace persistent import steps with ones in zcml.

--- trunk/tlspu/cookiepolicy/setuphandlers.py
+++ trunk/tlspu/cookiepolicy/setuphandlers.py
@@ -1,3 +1,5 @@
+import logging
+
 from Products.CMFCore.utils import getToolByName
 
 
@@ -29,14 +31,52 @@ def uninstall(context):
         return
 
     portal = context.getSite()
+    logger = context.getLogger('tlspu.cookiepolicy')
     portal_conf = getToolByName(portal, 'portal_controlpanel')
-    # Unregister our control panel configlet.
     portal_conf.unregisterConfiglet('cookiepolicy')
+    logger.info('Unregistered our cookiepolicy control panel configlet.')
 
     # Remove our properties from portal properties.
     pp = getToolByName(portal, 'portal_properties')
-    try:
-        if hasattr(pp, 'tlspu_cookiepolicy_properties'):
-            pp.manage_delObjects(ids='tlspu_cookiepolicy_properties')
-    except KeyError:
-        pass
+    if hasattr(pp, 'tlspu_cookiepolicy_properties'):
+        pp.manage_delObjects(ids='tlspu_cookiepolicy_properties')
+        logger.info('Removed tlspu_cookiepolicy_properties.')
+
+    # We may still have persistent steps.  Remove them.
+    remove_persistent_import_step(portal, logger=logger)
+
+
+def remove_persistent_import_step(context, logger=None):
+    """Import steps should not be persistent.
+
+    Defining them in import_steps.xml makes them persistent and may
+    break some Plone versions when you remove the product.
+
+    Instead, they should be defined in zcml.
+
+    Here we remove our old persistent import steps.
+    """
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger("tlspu.cookiepolicy")
+
+    # context may be portal_setup or the site root.
+    setup = getToolByName(context, 'portal_setup')
+    # context is portal_setup which is nice
+    registry = setup.getImportStepRegistry()
+    old_steps = [u"tlspu.cookiepolicy.install",
+                 u"tlspu.cookiepolicy.uninstall"]
+    for old_step in old_steps:
+        if old_step in registry.listSteps():
+            try:
+                registry.unregisterStep(old_step)
+            except AttributeError:
+                # BBB for GS 1.3
+                del registry._registered[old_step]
+
+            # Unfortunately we manually have to signal to portal_setup
+            # that it has changed otherwise this change is not
+            # persisted.
+            setup._p_changed = True
+            logger.info("Persistent %s import step removed from registry.",
+                        old_step)
