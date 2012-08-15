53ed94f160fee2eac95f8ca56ead50db07fa5c4e
maurits.vanrees <maurits.vanrees@b747bfa3-e8a4-06ba-1758-46ea45de51e8>
2012-08-15 16:25:02 +0000

    Fix uninstall and add tests for it.

--- tlspu/cookiepolicy/setuphandlers.py
+++ tlspu/cookiepolicy/setuphandlers.py
@@ -15,17 +15,26 @@ def install(context):
 
 
 def uninstall(context):
+    """Uninstall code.
+
+    Note that CMFQuickInstaller removes some stuff for us
+    automatically, though it is good form to remove it ourselves.  And
+    it does not undo everything.  Most things that it does not handle,
+    can be handled in GenericSetup xml files with a remove='True'
+    added, but this is not supported for all files.  In some files it
+    even breaks.  We handle those cases here.
+    """
 
     if context.readDataFile('tlspu.cookiepolicy_uninstall.txt') is None:
         return
 
-    #portal = context.getSite()
-    #portal_conf = getToolByName(portal, 'portal_controlpanel')
-    #portal_conf.unregisterConfiglet('@@likes-providers')
+    portal = context.getSite()
+    portal_conf = getToolByName(portal, 'portal_controlpanel')
+    # Unregister our control panel configlet.
+    portal_conf.unregisterConfiglet('cookiepolicy')
 
-    # Remove tweetmeme_properties in portal properties
-    pp = getToolByName('portal_properties')
-
+    # Remove our properties from portal properties.
+    pp = getToolByName(portal, 'portal_properties')
     try:
         if hasattr(pp, 'tlspu_cookiepolicy_properties'):
             pp.manage_delObjects(ids='tlspu_cookiepolicy_properties')
